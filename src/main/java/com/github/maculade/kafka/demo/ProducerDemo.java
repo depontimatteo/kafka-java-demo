25e7e0f8cba8f77a4a08ca57787caee3cf4b7fa9
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2021-01-01 19:20:54 +0100

    first commit

---
+++ src/main/java/com/github/maculade/kafka/demo/ProducerDemo.java
@@ -0,0 +1,8 @@
+package com.github.maculade.kafka.demo;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from the Producer!");
+    }
+}
