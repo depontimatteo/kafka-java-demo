8df770c00c68f957a68f3f4887437344f4a1c58a
depontimatteo <37115585+depontimatteo@users.noreply.github.com>
2021-01-04 17:08:19 +0100

    added consumer core functionality - processing data from the topic and insert records into elasticsearch

--- twitter-kafka-consumer-elasticsearch/src/main/java/kafka/elasticsearch/ElasticsearchConsumer.java
+++ twitter-kafka-consumer-elasticsearch/src/main/java/kafka/elasticsearch/ElasticsearchConsumer.java
@@ -6,6 +6,11 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,6 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class ElasticsearchConsumer {
 
@@ -48,24 +56,64 @@ public class ElasticsearchConsumer {
         return client;
     }
 
+    public static KafkaConsumer<String, String> createElasticsearchConsumer(){
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "kafka-demo-elaasticsearch";
+        String offsetConfig = "earliest";
+        String topic = "twitter-tweets";
+
+        // create consumer config
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetConfig);
+
+        // create consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singleton(topic));
+
+        return consumer;
+    }
+
     public static void main(String[] args) throws IOException {
 
         Logger logger = LoggerFactory.getLogger(ElasticsearchConsumer.class.getName());
 
         RestHighLevelClient client = createClient();
 
-        String jsonString = " { \"test\": \"figo\" }";
-        IndexRequest request = new IndexRequest(
-                "twitter",
-                "tweets"
-        ).source(jsonString, XContentType.JSON);
+        KafkaConsumer<String, String> elasticsearchConsumer = createElasticsearchConsumer();
 
-        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
-        String id = response.getId();
+        while(true){
 
-        logger.info("Id: " + id);
+            // I'm polling the topic with my consumer
 
-        client.close();
+            ConsumerRecords<String,String> records = elasticsearchConsumer.poll(Duration.ofMillis(100));
+
+            for(ConsumerRecord<String,String> record : records){
+
+                // And then for every record I put the JSON processed into Elasticsearch
+                IndexRequest request = new IndexRequest(
+                        "twitter",
+                        "tweets"
+                ).source(record.value(), XContentType.JSON);
+
+                IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+                String id = response.getId();
+
+                logger.info("Id: " + id);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //client.close();
 
     }
 }
